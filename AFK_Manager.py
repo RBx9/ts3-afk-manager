3f20b46851bee495f35e3c617d5409e84252e167
RBx9 <d4ngereux@gmail.com>
2026-01-21 18:35:44 +0200

    1.6

--- AFK_Manager.py
+++ AFK_Manager.py
@@ -12,7 +12,7 @@ import urllib.request
 import subprocess
 
 # --- PROJECT CONFIGURATION ---
-CURRENT_VERSION = "1.5"
+CURRENT_VERSION = "1.6"
 
 # DIRECT RAW LINKS TO YOUR REPO
 # Updated to match your new 'ts3-afk-manager' repo
